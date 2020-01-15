49d8d578c10c9522c2d406d0780b4d7e373195b4
Fumihiro_Maejima <testerwatson1101@gmail.com>
2020-01-16 00:13:44 +0900

    add movie page.

--- front/movie/src/main.js
+++ front/movie/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
     router,
--- front/movie/src/router.js
+++ front/movie/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import TestPage from './components/Pages/TestPage.vue'
+import MoviePage from './components/Pages/MoviePage.vue'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
             path: '/test',
             name: 'test',
             component: TestPage
+        },
+        {
+            path: '/movie',
+            name: 'movie',
+            component: MoviePage
         },
     ]
 })
