c43ff0e3528c2871cd2688983301e1125fb69c21
Fumihiro_Maejima <testerwatson1101@gmail.com>
2020-01-18 01:24:52 +0900

    add vuetify change.

---
+++ front/movie/src/components/Pages/MoviePage.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <TestSubModuleComponent module="subModule1"/>
+        <TestSubModuleComponent module="subModule2"/>
+        <TestModuleComponent/>
+        <!--<v-date-picker v-model="picker"></v-date-picker>-->
+    </div>
+</template>
+
+<script>
+import TestSubModuleComponent from './TestSubModuleComponent.vue'
+import TestModuleComponent from './TestModuleComponent.vue'
+
+export default {
+    name: 'app',
+    components: {
+        TestSubModuleComponent,
+        TestModuleComponent
+    },
+    data() {
+        return {
+            picker: null
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- front/movie/src/main.js
+++ front/movie/src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import Vuetify from 'vuetify'
+// import Vuetify from 'vuetify'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
-Vue.use(Vuetify)
+// Vue.use(Vuetify)
 
 new Vue({
     router,
     store,
-    render: h => h(App),
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
